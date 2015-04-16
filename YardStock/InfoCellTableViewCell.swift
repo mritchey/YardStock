44e881234161aaa4943fa9011ebff6b82c431d8f
reeschilling <reeschilling@gmail.com>
2015-04-15 20:37:56 -0500

    Finished all but report views
    Stockyard pages done aside from report views

---
+++ YardStock/InfoCellTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  InfoCellTableViewCell.swift
+//  YardStock
+//
+//  Created by Randall Lee Schilling on 4/15/15.
+//  Copyright (c) 2015 Group4. All rights reserved.
+//
+
+import UIKit
+
+class InfoCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var stockyardNameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var livestockLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- YardStock/RecentReportViewController.swift
+++ YardStock/RecentReportViewController.swift
@@ -12,12 +12,16 @@ class RecentReportViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var pageTitle: UINavigationItem!
-    
+
+    var rowsAdded: [NSIndexPath] = []
+    var segSelect: Int?
     var stockyard: Yard?
     
     //@IBOutlet weak var Title: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        segSelect = 1
+        rowsAdded.append(NSIndexPath(forRow: 2, inSection: 0))
         
         navigationController?.navigationBar.topItem?.title = ""
        
@@ -26,6 +30,8 @@ class RecentReportViewController: UIViewController {
         tableView.rowHeight = UITableViewAutomaticDimension
         
         // Do any additional setup after loading the view.
+//        InfoCellTableViewCell.nameLabel = stockyard.name
+        
     }
     
     //add nav title back
@@ -38,7 +44,18 @@ class RecentReportViewController: UIViewController {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        if segSelect == 0 {
+            return 3
+        }
+        else if segSelect == 1 {
+            return 3
+        }
+        else if segSelect == 2 {
+            return (2 + stockyard!.auctions.count)
+        }
+        else {
+            return 1
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -46,18 +63,34 @@ class RecentReportViewController: UIViewController {
         var cell: UITableViewCell?
         
         if indexPath.row == 0 {
-            cell = tableView.dequeueReusableCellWithIdentifier("infoCell", forIndexPath: indexPath) as? UITableViewCell
+            let infoCell = tableView.dequeueReusableCellWithIdentifier("infoCell", forIndexPath: indexPath) as? InfoCellTableViewCell
+            infoCell?.stockyardNameLabel.text = stockyard!.name
+            infoCell?.addressLabel.text = stockyard!.address + ", " + stockyard!.city
+            infoCell?.phoneLabel.text = stockyard!.contact
+            infoCell?.livestockLabel.text = stockyard!.auctions[0].livestock
+            cell = infoCell
         }
         
         else if indexPath.row == 1 {
             cell = tableView.dequeueReusableCellWithIdentifier("segControlCell", forIndexPath: indexPath) as? UITableViewCell
         }
         
-        else if indexPath.row == 2 {
-            cell = tableView.dequeueReusableCellWithIdentifier("reportCell", forIndexPath: indexPath) as? UITableViewCell
+        else if indexPath.row >= 2 {
+            if segSelect == 1 {
+                cell = tableView.dequeueReusableCellWithIdentifier("reportCell", forIndexPath: indexPath) as? UITableViewCell
+            }
+            else if segSelect == 0 {
+                cell = tableView.dequeueReusableCellWithIdentifier("pastReportsCell", forIndexPath: indexPath) as? UITableViewCell
+            }
+            else if segSelect == 2 {
+                cell = tableView.dequeueReusableCellWithIdentifier("upcomingAuctionsCell", forIndexPath: indexPath) as? UITableViewCell
+                cell!.textLabel!.text = stockyard!.auctions[indexPath.row - 2].livestock
+                cell!.detailTextLabel!.text = stockyard!.auctions[indexPath.row - 2].salesDate + " | " + stockyard!.auctions[indexPath.row - 2].time
+            }
         }
         
         return cell!
+        
         
     }
     
@@ -65,15 +98,40 @@ class RecentReportViewController: UIViewController {
     @IBAction func segChanged(sender: AnyObject) {
         let segControl = sender as! UISegmentedControl
         let selected = segControl.selectedSegmentIndex
+        var numCellsAdded = 0
+        var auctions: [NSIndexPath] = []
         
         if selected == 0 {
             pageTitle.title = "Past Reports"
+            segSelect = 0
+            tableView.beginUpdates()
+            tableView.deleteRowsAtIndexPaths(rowsAdded, withRowAnimation: .Fade)
+            tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 2, inSection: 0)], withRowAnimation: UITableViewRowAnimation.Top)
+            tableView.endUpdates()
+            rowsAdded = [NSIndexPath(forRow: 2, inSection: 0)]
         }
         else if selected == 1 {
             pageTitle.title = "Recent Report"
+            segSelect = 1
+            tableView.beginUpdates()
+            tableView.deleteRowsAtIndexPaths(rowsAdded, withRowAnimation: .Fade)
+            tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: 2, inSection: 0)], withRowAnimation: .Top)
+            tableView.endUpdates()
+            rowsAdded = [NSIndexPath(forRow: 2, inSection: 0)]
         }
         else if selected == 2 {
             pageTitle.title = "Upcoming Auctions"
+            segSelect = 2
+            tableView.beginUpdates()
+            tableView.deleteRowsAtIndexPaths(rowsAdded, withRowAnimation: .Fade)
+            
+            for i in 2...(1 + stockyard!.auctions.count) {
+                auctions.append(NSIndexPath(forRow: i, inSection: 0))
+            }
+                
+            tableView.insertRowsAtIndexPaths(auctions, withRowAnimation: .Top)
+            tableView.endUpdates()
+            rowsAdded = auctions
         }
     }
     
